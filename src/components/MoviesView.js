fa9349f6406c44177d3c72adcdcfd2be2f24e196
ViTaUM <vitaum.alves@gmail.com>
2023-11-17 09:27:52 -0300

    evento.json

--- src/components/MoviesView.js
+++ src/components/MoviesView.js
@@ -1,33 +1,14 @@
 import styled from "styled-components";
 import { useState, useEffect } from "react";
 import MoviesList from "./MoviesList";
-// import evento from "../evento/evento.json";
-import axios from "axios"; // Importe o Axios
+import evento from "../evento/evento.json";
 
 export default function MoviesView() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    // Cria a configuração dos cabeçalhos para o Axios
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-        "X-Requested-With": "XMLHttpRequest", // Adiciona o cabeçalho X-Requested-With
-        // Exemplo: Adiciona um cabeçalho de autorização
-        // 'Authorization': 'Bearer seu-token-aqui'
-        // Adicione outros cabeçalhos conforme necessário
-      },
-    };
-
-    // Faz uma chamada para o servidor backend para buscar os dados dos eventos usando Axios
-    axios
-      .get("http://localhost:3333/eventos", config)
-      .then((response) => {
-        setMovies(response.data); // O Axios já faz o parse do JSON automaticamente
-      })
-      .catch((error) => {
-        console.error("Erro ao buscar os eventos:", error);
-      });
+    // Definindo os filmes com os dados importados do JSON
+    setMovies(evento);
   }, []);
 
   return (
