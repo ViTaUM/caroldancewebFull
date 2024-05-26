698ec38550f1ab7d505ba0838f40fff157d2c0bc
ViTaUM <vitaum.alves@gmail.com>
2024-05-26 09:00:10 -0300

    liberando

--- src/components/Seat.js
+++ src/components/Seat.js
@@ -14,9 +14,9 @@ export default function Seat({
   function selectSeat(seatId, name, isSelected, isAvailable, valor) {
     // Contar assentos selecionados com valor 0 e 30
    // const countValor0 = selectedSeats.filter((seat) => seat.valor === 0).length;
-    const countValor30 = selectedSeats.filter(
-      (seat) => seat.valor === 30
-    ).length;
+    // const countValor30 = selectedSeats.filter(
+    //   (seat) => seat.valor === 30
+    // ).length;
 
     // Verifica se está tentando selecionar um novo assento
     // if (!isSelected && selectedSeats.length >= 5 && !avulso) {
@@ -34,12 +34,12 @@ export default function Seat({
     //   return;
     // }
 
-    if (countValor30 || valor !== 0) {
-      alert(
-        "Os ingressos estarão disponíveis para venda a partir das 09 horas."
-      );
-      return;
-    }
+    // if (countValor30 || valor !== 0) {
+    //   alert(
+    //     "Os ingressos estarão disponíveis para venda a partir das 09 horas."
+    //   );
+    //   return;
+    // }
 
     if (isSelected) {
       // Remover o assento desmarcado com base no seatId
